61575a47a7ce77960e0ded2bf9e041524a0a6bd5
Mateusz Kuźniarek <210245@edu.p.lodz.pl>
2020-03-11 04:08:47 +0100

    Initial project

---
+++ ImageProcessing/ImageProcessing/MainWindowController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace ImageProcessing
+{
+    public class MainWindowController
+    {
+        public ICommand LoadImageCommand { get; private set; }
+        public ICommand SaveImageCommand { get; private set; }
+
+        public BitmapImage SelectedImage { get; set; }
+        public ObservableCollection<BitmapImage> Images { get; private set; } = new ObservableCollection<BitmapImage>();
+
+        public MainWindowController()
+        {
+            LoadImageCommand = new RelayCommand(x => LoadImage());
+            SaveImageCommand = new RelayCommand(x => SaveImage());
+        }
+
+        private void LoadImage()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == false) return;
+            Images.Add(new BitmapImage(new Uri(dlg.FileName)));
+        }
+
+        private void SaveImage()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Bitmap Image(.bmp)| *.bmp";
+            if (saveFileDialog.ShowDialog() == false) return; BitmapEncoder encoder = new PngBitmapEncoder();
+
+            encoder.Frames.Add(BitmapFrame.Create(SelectedImage));
+            using (var fileStream = new System.IO.FileStream(saveFileDialog.FileName, System.IO.FileMode.Create))
+            {
+                encoder.Save(fileStream);
+            }
+        }
+    }
+}
---
+++ ImageProcessing/ImageProcessing/UriConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ImageProcessing
+{
+    public class UriConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Uri uri = value as Uri;
+            return uri.Segments.Last();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
